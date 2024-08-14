6d8649f59048d711dacc3ff24217db3b42fb65ac
Aishwarya Kundur <84147216+AishwaryaCodes@users.noreply.github.com>
2024-08-14 15:04:38 -0500

    Initial commit

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+import { Fade } from "react-reveal";
+
+export default function Footer(props) {
+  return (
+    <div className="footer-div">
+      <Fade>
+        <p className="footer-text" style={{ color: props.theme.secondaryText }}>
+          Code & Conquer! - Designed by AKundur 
+        </p>
+        {/* <ToggleSwitch theme={props.theme} onToggle={props.onToggle}/> */}
+      </Fade>
+    </div>
+  );
+}
---
+++ src/containers/Main.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import Home from "../pages/home/HomeComponent";
+import Education from "../pages/education/EducationComponent";
+import Experience from "../pages/experience/Experience";
+import Contact from "../pages/contact/ContactComponent";
+import Projects from "../pages/projects/Projects";
+import Error404 from "../pages/errors/error404/Error";
+
+export default class Main extends Component {
+  render() {
+    return (
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Switch>
+        <Route
+            path="/" 
+            exact
+            render={(props) => <Home {...props} theme={this.props.theme} />}
+          />
+          <Route
+            path="/home" 
+            exact
+            render={(props) => <Home {...props} theme={this.props.theme} />}
+          />
+          <Route
+            path="/experience"
+            exact
+            render={(props) => (
+              <Experience {...props} theme={this.props.theme} />
+            )}
+          />
+          <Route
+            path="/education"
+            render={(props) => (
+              <Education {...props} theme={this.props.theme} />
+            )}
+          />
+          <Route
+            path="/contact"
+            render={(props) => <Contact {...props} theme={this.props.theme} />}
+          />
+          <Route
+            path="/projects"
+            render={(props) => <Projects {...props} theme={this.props.theme} />}
+          />
+          <Route
+            path="*"
+            render={(props) => <Error404 {...props} theme={this.props.theme} />}
+          />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
